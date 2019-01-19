c624d74567f76f0534cd2ac9f8097a55648f9621
Dmitry Kulagin <kulaginds@gmail.com>
2019-01-19 13:24:06 +0300

    realized validation in Url model; realized translate ID from decimal to own ABC in Url model

--- turl/models.go
+++ turl/models.go
@@ -1,6 +1,65 @@
 package turl
 
+import (
+	"math"
+	"net/url"
+	"strings"
+)
+
+const (
+	noErrors uint = 0
+	errNotValidUrl uint = 1
+	errEmptyUrl uint = 2
+	errUrlTooLong uint = 3
+)
+
+const (
+	maxUrlLength = 2000
+
+	ABC = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
 type Url struct {
-	Id  uint64
+	Id  int
 	Url string
+}
+
+func (u *Url) Validate() (errorCode uint, ok bool) {
+	var length = uint(len(u.Url))
+
+	if 0 == length {
+		errorCode, ok = errEmptyUrl, false
+		return
+	}
+
+	if length > maxUrlLength {
+		errorCode, ok = errUrlTooLong, false
+		return
+	}
+
+	_, err := url.ParseRequestURI(u.Url)
+
+	if nil != err {
+		errorCode, ok = errNotValidUrl, false
+	}
+
+	return noErrors, true
+}
+
+// Return Id in own ABC
+// Read from left to right
+func (u *Url) GetABCId() string {
+	var quotient, remainder = u.Id, 0
+	base := len(ABC)
+	maxABCIdLength := int(math.Ceil(math.Log2(float64(u.Id)) / math.Log2(float64(base))))
+	chars := make([]string, maxABCIdLength)
+
+	for i := 0; i < maxABCIdLength; i++ {
+		remainder = quotient % base
+		quotient = quotient / base
+
+		chars[i] = string(ABC[remainder])
+	}
+
+	return strings.Join(chars, "")
 }
