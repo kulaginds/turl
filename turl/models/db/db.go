036ffe808b40af980daf05d74a6bf372b03ac223
Dmitry Kulagin <kulaginds@gmail.com>
2019-01-24 22:56:24 +0300

    moved func CheckStruct from simple to db

--- turl/models/db/db.go
+++ turl/models/db/db.go
@@ -1,5 +1,10 @@
 package db
 
+import (
+	"fmt"
+	"os"
+)
+
 const (
 	dbDriver = "mysql"
 	urlsTable = "urls"
@@ -32,4 +37,50 @@ func NewDB(dsn string, shardCount uint16) (d DB, ok bool) {
 	}
 
 	return
+}
+
+func CheckStruct(columns []*TableColumnStruct) (ok bool) {
+	if ok = checkStructId(columns[0]); !ok {
+		fmt.Fprintln(os.Stderr, "Wrong structure of id field in table urls")
+		return
+	}
+
+	if ok = checkStructUrl(columns[1]); !ok {
+		fmt.Fprintln(os.Stderr, "Wrong structure of url field in table urls")
+		return
+	}
+
+	return true
+}
+
+func checkStructId(idColumn *TableColumnStruct) (ok bool) {
+	if "id" != *idColumn.Field {
+		return
+	}
+
+	if "int(10) unsigned" != *idColumn.Type {
+		return
+	}
+
+	if "PRI" != *idColumn.Key {
+		return
+	}
+
+	if "auto_increment" != *idColumn.Extra {
+		return
+	}
+
+	return true
+}
+
+func checkStructUrl(urlColumn *TableColumnStruct) (ok bool) {
+	if "url" != *urlColumn.Field {
+		return
+	}
+
+	if "text" != *urlColumn.Type {
+		return
+	}
+
+	return true
 }
--- turl/models/db/simple.go
+++ turl/models/db/simple.go
@@ -76,7 +76,7 @@ func (d *SimpleDB) Initialize() (ok bool) {
 		return
 	}
 
-	return d.checkStruct(columns)
+	return CheckStruct(columns)
 }
 
 func (d *SimpleDB) createUrlsTable() (ok bool) {
@@ -93,52 +93,6 @@ func (d *SimpleDB) createUrlsTable() (ok bool) {
 	if nil != err {
 		fmt.Fprintln(os.Stderr, "Can't create table urls")
 		fmt.Fprintln(os.Stderr, err.Error())
-		return
-	}
-
-	return true
-}
-
-func (d *SimpleDB) checkStruct(columns []*TableColumnStruct) (ok bool) {
-	if ok = d.checkStructId(columns[0]); !ok {
-		fmt.Fprintln(os.Stderr, "Wrong structure of id field in table urls")
-		return
-	}
-
-	if ok = d.checkStructUrl(columns[1]); !ok {
-		fmt.Fprintln(os.Stderr, "Wrong structure of url field in table urls")
-		return
-	}
-
-	return true
-}
-
-func (d *SimpleDB) checkStructId(idColumn *TableColumnStruct) (ok bool) {
-	if "id" != *idColumn.Field {
-		return
-	}
-
-	if "int(10) unsigned" != *idColumn.Type {
-		return
-	}
-
-	if "PRI" != *idColumn.Key {
-		return
-	}
-
-	if "auto_increment" != *idColumn.Extra {
-		return
-	}
-
-	return true
-}
-
-func (d *SimpleDB) checkStructUrl(urlColumn *TableColumnStruct) (ok bool) {
-	if "url" != *urlColumn.Field {
-		return
-	}
-
-	if "text" != *urlColumn.Type {
 		return
 	}
 
